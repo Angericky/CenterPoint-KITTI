8cc1123df86330def8519620f83bf2193d4e2ab9
angericky <jingjingbudlet@gmail.com>
2021-07-16 10:07:40 +0800

    modify base_bev_backbone

--- pcdet/models/backbones_2d/base_bev_backbone.py
+++ pcdet/models/backbones_2d/base_bev_backbone.py
@@ -4,32 +4,34 @@ import torch.nn as nn
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_filters, out_filters, stride=1, padding=1, pooling=True, pooling_all=True):
+    def __init__(self, in_filters, out_filters, stride=1, padding=1):
         super(ResBlock, self).__init__()
 
         self.conv1_3x1 = nn.Conv2d(in_filters, out_filters, kernel_size=(3,1),
-                        stride=stride, padding=(padding, 0), bias=False)
+                        stride=1, padding=(padding, 0), bias=False)
         self.bn1_1 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
         self.relu1_1 = nn.ReLU()
 
         self.conv1_1x3 = nn.Conv2d(out_filters, out_filters, kernel_size=(1,3),
-                        stride=stride, padding=(0, padding), bias=False)
+                        stride=1, padding=(0, padding), bias=False)
         self.bn1_2 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
         self.relu1_2 = nn.ReLU()
 
         self.conv2_3x1 = nn.Conv2d(in_filters, out_filters, kernel_size=(3,1),
-                        stride=stride, padding=(padding, 0), bias=False)
+                        stride=1, padding=(padding, 0), bias=False)
         self.bn2_1 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
         self.relu2_1 = nn.ReLU()
 
         self.conv2_1x3 = nn.Conv2d(out_filters, out_filters, kernel_size=(1,3),
-                        stride=stride, padding=(0, padding), bias=False)
+                        stride=1, padding=(0, padding), bias=False)
         self.bn2_2 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
         self.relu2_2 = nn.ReLU()
         
-        if pooling:
-            self.pool = nn.Conv2d(out_filters, out_filters, kernel_size=(3,3),
-                                stride=2, padding=padding, bias=False)
+        # self.pooling = False
+        # if stride == 2:
+        self.pool = nn.Conv2d(out_filters, out_filters, kernel_size=(3,3),
+                                stride=stride, padding=padding, bias=False)
+            # self.pooling = True
             
         self.weight_initialization()
 
@@ -39,7 +41,7 @@ class ResBlock(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x):
+    def forward(self, x, **args):
         shortcut = self.conv1_3x1(x)
         shortcut = self.bn1_1(shortcut)
         shortcut = self.relu1_1(shortcut)
@@ -57,32 +59,36 @@ class ResBlock(nn.Module):
         res = self.relu2_2(res)
 
         res = res + shortcut
+        
+        resB = self.pool(res)
+
+        return resB
             
         return res
 
 
 class UpBlock(nn.Module):
-    def __init__(self, in_filters, out_filters, kernel_size=(3, 3, 3), indice_key=None, up_key=None):
+    def __init__(self, in_filters, out_filters, stride=1, kernel_size=(3, 3, 3), indice_key=None, up_key=None):
         super(UpBlock, self).__init__()
         # self.drop_out = drop_out
-        self.trans_dilao = conv3x3(in_filters, out_filters, indice_key=indice_key + "new_up")
+        self.trans_dilao = nn.ConvTranspose2d(in_filters, out_filters, kernel_size=(3,3), stride=stride, padding=1, bias=False)
         self.trans_act = nn.LeakyReLU()
-        self.trans_bn = nn.BatchNorm1d(out_filters)
+        self.trans_bn = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
 
-        self.conv1 = conv1x3(out_filters, out_filters, indice_key=indice_key)
+        self.conv1 = nn.ConvTranspose2d(out_filters, out_filters, kernel_size=(1,3), stride=1, padding=(0,1), bias=False)
         self.act1 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm1d(out_filters)
+        self.bn1 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
 
-        self.conv2 = conv3x1(out_filters, out_filters, indice_key=indice_key)
+        self.conv2 = nn.ConvTranspose2d(out_filters, out_filters, kernel_size=(3,1), stride=1, padding=(1,0), bias=False)
         self.act2 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm1d(out_filters)
+        self.bn2 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
 
-        self.conv3 = conv3x3(out_filters, out_filters, indice_key=indice_key)
+        self.conv3 = nn.ConvTranspose2d(out_filters, out_filters, kernel_size=(3,3), stride=1, padding=1, bias=False)
         self.act3 = nn.LeakyReLU()
-        self.bn3 = nn.BatchNorm1d(out_filters)
+        self.bn3 = nn.BatchNorm2d(out_filters, eps=1e-3, momentum=0.01)
         # self.dropout3 = nn.Dropout3d(p=dropout_rate)
 
-        self.up_subm = spconv.SparseInverseConv3d(out_filters, out_filters, kernel_size=3, indice_key=up_key,
+        self.up_subm = nn.ConvTranspose2d(out_filters, out_filters, 2,
                                                   bias=False)
 
         self.weight_initialization()
@@ -93,27 +99,26 @@ class UpBlock(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def forward(self, x, skip):
+    def forward(self, x):
         upA = self.trans_dilao(x)
-        upA.features = self.trans_act(upA.features)
-        upA.features = self.trans_bn(upA.features)
+        upA = self.trans_act(upA)
+        upA = self.trans_bn(upA)
 
         ## upsample
         upA = self.up_subm(upA)
-
-        upA.features = upA.features + skip.features
-
+        import pdb
+        pdb.set_trace()
         upE = self.conv1(upA)
-        upE.features = self.act1(upE.features)
-        upE.features = self.bn1(upE.features)
+        upE = self.act1(upE)
+        upE = self.bn1(upE)
 
         upE = self.conv2(upE)
-        upE.features = self.act2(upE.features)
-        upE.features = self.bn2(upE.features)
+        upE = self.act2(upE)
+        upE = self.bn2(upE)
 
         upE = self.conv3(upE)
-        upE.features = self.act3(upE.features)
-        upE.features = self.bn3(upE.features)
+        upE = self.act3(upE)
+        upE = self.bn3(upE)
 
         return upE
     
@@ -155,7 +160,7 @@ class AsymmBEVBackbone(nn.Module):
 
             for k in range(layer_nums[idx]):
                 cur_layers.extend([
-                    ResBlock(num_filters[idx], num_filters[idx], padding=1)
+                    ResBlock(num_filters[idx], num_filters[idx])
                 ])
 
             self.blocks.append(nn.Sequential(*cur_layers))
@@ -164,33 +169,27 @@ class AsymmBEVBackbone(nn.Module):
                 stride = upsample_strides[idx]
                 if stride >= 1:
                     self.deblocks.append(nn.Sequential(
-                        nn.ConvTranspose2d(
-                            num_filters[idx], num_upsample_filters[idx],
-                            upsample_strides[idx],
-                            stride=upsample_strides[idx], bias=False
-                        ),
-                        nn.BatchNorm2d(num_upsample_filters[idx], eps=1e-3, momentum=0.01),
-                        nn.ReLU()
+                        UpBlock(num_filters[idx], num_upsample_filters[idx], stride=stride)
                     ))
-                else:
-                    stride = np.round(1 / stride).astype(np.int)
-                    self.deblocks.append(nn.Sequential(
-                        nn.Conv2d(
-                            num_filters[idx], num_upsample_filters[idx],
-                            stride,
-                            stride=stride, bias=False
-                        ),
-                        nn.BatchNorm2d(num_upsample_filters[idx], eps=1e-3, momentum=0.01),
-                        nn.ReLU()
-                    ))
+                # else:
+                #     stride = np.round(1 / stride).astype(np.int)
+                #     self.deblocks.append(nn.Sequential(
+                #         nn.Conv2d(
+                #             num_filters[idx], num_upsample_filters[idx],
+                #             stride,
+                #             stride=stride, bias=False
+                #         ),
+                #         nn.BatchNorm2d(num_upsample_filters[idx], eps=1e-3, momentum=0.01),
+                #         nn.ReLU()
+                #     ))
 
         c_in = sum(num_upsample_filters)
-        if len(upsample_strides) > num_levels:
-            self.deblocks.append(nn.Sequential(
-                nn.ConvTranspose2d(c_in, c_in, upsample_strides[-1], stride=upsample_strides[-1], bias=False),
-                nn.BatchNorm2d(c_in, eps=1e-3, momentum=0.01),
-                nn.ReLU(),
-            ))
+        # if len(upsample_strides) > num_levels:
+        #     self.deblocks.append(nn.Sequential(
+        #         nn.ConvTranspose2d(c_in, c_in, upsample_strides[-1], stride=upsample_strides[-1], bias=False),
+        #         nn.BatchNorm2d(c_in, eps=1e-3, momentum=0.01),
+        #         nn.ReLU(),
+        #     ))
 
         self.num_bev_features = c_in
 
@@ -205,7 +204,7 @@ class AsymmBEVBackbone(nn.Module):
         ups = []
         ret_dict = {}
         x = spatial_features
-
+        
         for i in range(len(self.blocks)):
             x = self.blocks[i](x)
 
@@ -223,7 +222,9 @@ class AsymmBEVBackbone(nn.Module):
 
         if len(self.deblocks) > len(self.blocks):
             x = self.deblocks[-1](x)
-
+        
+        import pdb
+        pdb.set_trace()
         data_dict['spatial_features_2d'] = x
 
         return data_dict
