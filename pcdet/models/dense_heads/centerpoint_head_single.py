014e56b6385c979bc5dbeeb92a7f1fa3f16621cc
angericky <jingjingbudlet@gmail.com>
2021-09-09 01:09:29 +0800

    delete rho log

--- pcdet/models/dense_heads/centerpoint_head_single.py
+++ pcdet/models/dense_heads/centerpoint_head_single.py
@@ -234,27 +234,27 @@ class CenterHead(nn.Module):
 
         ### Visualization end.
 
-        # flat_box_preds = box_preds.view(box_preds.shape[0], -1, box_preds.shape[-1]).clone()
-        # batch_size = data_dict['batch_size']
-        # for i in range(batch_size):
-        #    targets_dict['anno_boxes'][0][i][..., -1] = targets_dict['anno_boxes'][0][i][..., -1] + 0.001 
-        #    flat_box_preds[i, targets_dict['inds'][0][i]] = targets_dict['anno_boxes'][0][i]
+        flat_box_preds = box_preds.view(box_preds.shape[0], -1, box_preds.shape[-1]).clone()
+        batch_size = data_dict['batch_size']
+        for i in range(batch_size):
+           targets_dict['anno_boxes'][0][i][..., -1] = targets_dict['anno_boxes'][0][i][..., -1] + 0.001 
+           flat_box_preds[i, targets_dict['inds'][0][i]] = targets_dict['anno_boxes'][0][i]
         
         if not self.training or self.predict_boxes_when_training:
             batch_cls_preds, batch_box_preds = self.generate_predicted_boxes(
                 batch_size=data_dict['batch_size'],
-                cls_preds=cls_preds, box_preds=box_preds, dir_cls_preds=None
-                # cls_preds=cls_preds, box_preds=flat_box_preds.view(box_preds.shape), dir_cls_preds=None
+                # cls_preds=cls_preds, box_preds=box_preds, dir_cls_preds=None
+                cls_preds=cls_preds, box_preds=flat_box_preds.view(box_preds.shape), dir_cls_preds=None
             )
-            # data_dict['batch_cls_preds'] = targets_dict['heatmaps'][0].view(batch_size, 3, -1).permute(0, 2, 1)
+            data_dict['batch_cls_preds'] = targets_dict['heatmaps'][0].view(batch_size, 3, -1).permute(0, 2, 1)
             # print(data_dict['batch_cls_preds'][0][89219])
             # print(batch_box_preds[0][89219])
             # import pdb
             # pdb.set_trace()
-            data_dict['batch_cls_preds'] = batch_cls_preds
+            # data_dict['batch_cls_preds'] = batch_cls_preds
             data_dict['batch_box_preds'] = batch_box_preds
             data_dict['cls_preds_normalized'] = False
-            # data_dict['cls_preds_normalized'] = True
+            data_dict['cls_preds_normalized'] = True
 
         return data_dict
 
@@ -595,7 +595,7 @@ class CenterHead(nn.Module):
                                         dtype=torch.float32),
                             arc * r.unsqueeze(0),
                             z.unsqueeze(0), 
-                            corner_offset[0:1],
+                            corner_offset[0:1] / 3,
                             #sigmoid_corner_phi,
                             corner_offset[1:2] * r,
                             height_dim,
@@ -611,14 +611,14 @@ class CenterHead(nn.Module):
                         
                         # with open('arc_gt.txt', 'a+') as f:
                         #      f.write('{}\n'.format((corner_phi_offset * r * 0.0064).item()))
-                        with open('arc_gt_rad.txt', 'a+') as f:
-                             f.write('{}\n'.format((corner_offset[1:2] * r).item()))
+                        # with open('arc_gt_rad.txt', 'a+') as f:
+                        #      f.write('{}\n'.format((corner_offset[1:2] * r).item()))
                         # print('anno_box: ', anno_box[new_idx][3].item(), anno_box[new_idx][4].item())
 
 
-                        if corner_offset[0:1].item() - 1 > 0:        
-                            import pdb
-                            pdb.set_trace()
+                        # if corner_offset[0:1].item() - 1 > 0:        
+                        #     import pdb
+                        #     pdb.set_trace()
                         # center[1] = arc * r / rho + center[1]
                         # corner = corner_offset + center
 
@@ -736,7 +736,7 @@ class CenterHead(nn.Module):
             #sigmoid_phi = corner[:, :, 1:2] + 0.5
             #inverse_sigmoid_phi = torch.log(sigmoid_phi / (1 - sigmoid_phi))
 
-            corner_rho = corner[:, :, 0:1] + (voxel_cx + batch_reg[:, :, 0:1]) * self.target_cfg.OUT_SIZE_FACTOR * cylind_size[0] + cylind_range[0]
+            corner_rho = corner[:, :, 0:1] * 3 + (voxel_cx + batch_reg[:, :, 0:1]) * self.target_cfg.OUT_SIZE_FACTOR * cylind_size[0] + cylind_range[0]
             corner_phi = corner[:, :, 1:2] / rho + (voxel_cy) * self.target_cfg.OUT_SIZE_FACTOR * cylind_size[1] + cylind_range[1] + angle_offset
 
             corner_xy = torch.cat((corner_rho * torch.cos(corner_phi), corner_rho * torch.sin(corner_phi)), axis=2).view(-1, 2)
@@ -890,6 +890,8 @@ class CenterHead(nn.Module):
         # yaw_loss = l1_loss(
         #     pred[:, :, -1:], target_box[:, :, -1:], bbox_weights[:, :, -1:], avg_factor=(num + 1e-4))
 
+
+        target_box[:, :, 3:4] = target_box[:, :, 3:4] / 3
 
         loc_loss = self.reg_loss_func(
             pred, target_box, bbox_weights, avg_factor=(num + 1e-4))
